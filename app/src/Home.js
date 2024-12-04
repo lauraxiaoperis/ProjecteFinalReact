8feb61d91fd5e5d5c99776cfe3494a6185667d0e
lauraxiaoperis <160018903+lauraxiaoperis@users.noreply.github.com>
2024-12-04 13:09:01 +0100

    Home filtered OK

--- app/src/Home.js
+++ app/src/Home.js
@@ -4,17 +4,18 @@ import './App.css';
 
 function DigimonsList() {
     const [digimons, setDigimons] = useState([]); // Estado para almacenar todos los Digimons
-    const [filteredAttributeName, setFilteredAttributeName] = useState([]); 
+    const [filteredAttributeName, setFilteredAttributeName] = useState([]); //Array con los tipos de atributos que hay
+    const [randomAttribute, setRandomAttribute] = useState(''); // Atributo aleatorio
     const [filteredDigimons, setFilteredDigimons] = useState([]); // Estado para almacenar los Digimons filtrados por atributo
 
     const numberDigimons = 20;
-    // Array con las URLs de las APIs
+    // APIs URL
     const apiUrls = {
         digimons: `https://digi-api.com/api/v1/digimon?pageSize=${numberDigimons}`,
         attribute: 'https://digi-api.com/api/v1/attribute',
     };
     
-    // General - Digimon List
+    // =============== Fetch - General - Digimon List =============== 
     useEffect(() => {
         // Solicitar Digimons generales
         fetch(apiUrls.digimons)
@@ -27,24 +28,47 @@ function DigimonsList() {
             });
     }, []); 
 
-    // Random Attribute - Digimon List
+    // =============== Fetch - Digimon Attribute  =============== 
     useEffect(() => {
         fetch(apiUrls.attribute)
             .then((response) => response.json())
             .then((data) => {
-                if(data.content && data.content.fields){
+                if (data.content && data.content.fields) {
+                    // Obtener los nombres de los atributos
                     const attributeNames = data.content.fields.map(field => field.name);
                     setFilteredAttributeName(attributeNames);
                 }
             })
             .catch((error) => {
-                console.error('Error fetching Digimons:', error);
+                console.error('Error fetching Digimons attributes:', error);
             });
     }, []); 
+    
+    // =============== Fetch - Digimon List - Random Attribute  =============== 
+    useEffect(() => {
+        if (filteredAttributeName.length > 0) {
+            // Random Attribute
+            const ranAttribute = filteredAttributeName[Math.floor(Math.random() * filteredAttributeName.length)];
+            console.log("Random Attribute:", ranAttribute); 
+            setRandomAttribute(ranAttribute);
+    
+            // Realiza el fetch para obtener los Digimons basados en el atributo
+            fetch(`https://digi-api.com/api/v1/digimon?attribute=${ranAttribute}&&pageSize=${numberDigimons}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setFilteredDigimons(data.content || []);
+                })
+                .catch((error) => {
+                    console.error('Error fetching Random Digimons List:', error);
+                });
+        }
+    }, [filteredAttributeName]); // This useEffect runs every time filteredAttributeName changes
+    
+    
 
     return (
         <div>
-            {/* Llista inicial de Digimons */}
+            {/*  =============== General Digimon List ===============  */}
             <h2>Lista de Digimons</h2>
             <div className="digimons-container">
                 {digimons.length > 0 ? (
@@ -61,8 +85,21 @@ function DigimonsList() {
                 )}
             </div>
 
-            {/* Llista de Digimons filtrados por atributo */}
-            <h2>Digimons filtrados por atributo: {filteredAttributeName || 'Ninguno'}</h2>
+            {/* =============== Random Attribute - Digimon List =============== */}
+            <h2>Digimons filtered by attribute: {randomAttribute || 'Ninguno'}</h2>
+            <div className="digimons-container">
+            {filteredDigimons.length > 0 ? (
+                    filteredDigimons.map((digimon) => (
+                        <DigimonCard
+                            key={digimon.id} 
+                            title={digimon.name} 
+                            imageUrl={digimon.image} 
+                        />
+                    ))
+                ) : (
+                    <p>There are no Digimons with this attribute.</p> 
+                )}
+            </div>
         </div>
     );
 }
