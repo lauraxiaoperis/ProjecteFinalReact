c7a6ee00b4efd11f267b995518d2bb38de26505f
lauraxiaoperis <160018903+lauraxiaoperis@users.noreply.github.com>
2024-12-05 16:53:16 +0100

    detail update

--- app/src/DigimonDetail.js
+++ app/src/DigimonDetail.js
@@ -22,15 +22,45 @@ function DigimonDetail(){
     useEffect(() => {
         console.log('Digimon Detail actualizado:', digimonDetail);
     }, [digimonDetail]);
+
+    // =============== Print =============== 
     return(
         <div>
-            <h2>Digimon Detail</h2>
             {digimonDetail ? (
                 <div>
-                    <h3>{digimonDetail.name}</h3> 
+                    <h3>{digimonDetail.name}</h3>
+                    {digimonDetail.images?.[0] && ( //? comprova si existeix el primer element de images
+                        <img
+                            src={digimonDetail.images[0].href}
+                            alt={digimonDetail.name}
+                        />
+                    )}
+                    {digimonDetail.levels?.[0] && (
+                        <p>Level: {digimonDetail.levels[0].level}</p>
+                    )}
+                    {digimonDetail.types?.[0] && (
+                        <p>Type: {digimonDetail.types[0].type}</p>
+                    )}
+                    {digimonDetail.attributes?.length > 0 && (
+                        <p>Attributes: {digimonDetail.attributes.map(attribute => attribute.attribute).join(', ')}</p> 
+                    )}
+                    {digimonDetail.fields?.length > 0 && (
+                        <div>
+                            <p>Field:</p>
+                            {digimonDetail.fields.map((item, index) => (
+                                <div key={index}>
+                                    <img
+                                        src={item.image}
+                                        alt={item.field}
+                                    />
+                                    <span>{item.field}</span>
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             ) : (
-                <p>There are no Digimons with this attribute.</p> 
+                <p>There are no Details in this Digimon.</p> 
             )}
         </div>
     );
