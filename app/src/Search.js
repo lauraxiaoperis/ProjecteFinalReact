8f7363a0d94874ac18d94fbb8b4726e5b43ab7e4
GilBiFr <gil.bilurbina@estudiantat.upc.edu>
2024-12-05 12:20:13 +0100

    Search una miqueta

--- app/src/Search.js
+++ app/src/Search.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import DigimonContext from './DigimonContext';  // Importamos el contexto
+import './App.css';
 
 function Search(){
+    const [filteredDigimons, setFilteredDigimons] = useState([]); // Estado para almacenar los Digimons filtrados por atributo
+    const { filteredAttributeName } = useContext(DigimonContext); // Accedemos al contexto solo para los atributos
     return(
         <div>Search</div>
+        
     );
 }
 
