fe00a242972b3395d2f5fc9f6daa1f4d20142971
lauraxiaoperis <160018903+lauraxiaoperis@users.noreply.github.com>
2024-12-12 11:22:03 +0100

    css una mica

--- app/src/DigimonDetail.js
+++ app/src/DigimonDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import './Detail.css'; 
 
 function DigimonDetail(){
     const [digimonDetail, setDigimonDetail] = useState([]); // Estado para almacenar el Detalle del Digimon
@@ -26,51 +27,55 @@ function DigimonDetail(){
     return(
         <div>
             {digimonDetail ? (
-                <div>
-                    <h3>{digimonDetail.name}</h3>
-                    {digimonDetail.descriptions?.[1] && (
-                        <p>{digimonDetail.descriptions[1].description}</p>
-                    )}
-                    {digimonDetail.images?.[0] && ( //? comprova si existeix el primer element de images
-                        <img
-                            src={digimonDetail.images[0].href}
-                            alt={digimonDetail.name}
-                        />
-                    )}
-                    {digimonDetail.levels?.[0] && (
-                        <p>Level: {digimonDetail.levels[0].level}</p>
-                    )}
-                    {digimonDetail.types?.[0] && (
-                        <p>Type: {digimonDetail.types[0].type}</p>
-                    )}
-                    {digimonDetail.attributes?.length > 0 && (
-                        <p>Attributes: {digimonDetail.attributes.map(attribute => attribute.attribute).join(', ')}</p> 
-                    )}
-                    {digimonDetail.fields?.length > 0 && (
-                        <div>
-                            <p>Field:</p>
-                            {digimonDetail.fields.map((item, index) => (
-                                <div key={index}>
-                                    <img
-                                        src={item.image}
-                                        alt={item.field}
-                                    />
-                                    <span>{item.field}</span>
-                                </div>
-                            ))}
-                        </div>
-                    )}
-                    {digimonDetail.skills?.[0] && (
-                        <div>
-                            <p>skills:</p>
-                            {digimonDetail.skills.map((item, index) => (
-                                <div key={index}>
-                                    <p><strong>{item.skill}</strong> : {item.description}</p>
-                                </div>
-                            ))}
-                        </div>
+                <div id="digimon-detail">
+                    <div className='digimon-detail-image'>
+                        {digimonDetail.images?.[0] && ( //? comprova si existeix el primer element de images
+                            <img 
+                                src={digimonDetail.images[0].href}
+                                alt={digimonDetail.name}
+                            />
+                        )}
+                    </div>
+                    <div className='digimon-detail-info'>
+                        <h3>{digimonDetail.name}</h3>
+                        {digimonDetail.descriptions?.[1] && (
+                            <p id="description">{digimonDetail.descriptions[1].description}</p>
+                        )}
+                        {digimonDetail.levels?.[0] && (
+                            <p>Level: {digimonDetail.levels[0].level}</p>
+                        )}
+                        {digimonDetail.types?.[0] && (
+                            <p>Type: {digimonDetail.types[0].type}</p>
+                        )}
+                        {digimonDetail.attributes?.length > 0 && (
+                            <p>Attributes: {digimonDetail.attributes.map(attribute => attribute.attribute).join(', ')}</p> 
+                        )}
+                        {digimonDetail.fields?.length > 0 && (
+                            <div>
+                                <p>Field:</p>
+                                {digimonDetail.fields.map((item, index) => (
+                                    <div key={index}>
+                                        <img
+                                            src={item.image}
+                                            alt={item.field}
+                                        />
+                                        <span>{item.field}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                        {digimonDetail.skills?.[0] && (
+                            <div>
+                                <p>skills:</p>
+                                {digimonDetail.skills.map((item, index) => (
+                                    <div key={index}>
+                                        <p><strong>{item.skill}</strong> : {item.description}</p>
+                                    </div>
+                                ))}
+                    </div>
                     )}
                 </div>
+            </div>
             ) : (
                 <p>There are no Details in this Digimon.</p> 
             )}
